468ee9993499820b9b827921587e50f2f676dd77
Youssef EL HAMRI <youssefelhamri.2002@gmail.com>
2025-01-15 13:55:22 +0100

    Ajout des fonctions de la connexion à MongoDB et Redis dans le module db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,7 @@
 // Question : Pourquoi créer un module séparé pour les connexions aux bases de données ?
-// Réponse : 
+// Réponse : Pour assurer un couplage faible dans le cas de changement de moteur des bases de données 
 // Question : Comment gérer proprement la fermeture des connexions ?
-// Réponse : 
-
+// Réponse : On utilisant un block finally dans un try catch afin d'assurer de la fermeture de la connexion dans tous les cas
 const { MongoClient } = require('mongodb');
 const redis = require('redis');
 const config = require('./env');
@@ -10,16 +9,34 @@ const config = require('./env');
 let mongoClient, redisClient, db;
 
 async function connectMongo() {
-  // TODO: Implémenter la connexion MongoDB
-  // Gérer les erreurs et les retries
+  try {
+    mongoClient = new MongoClient(config.mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoClient.connect();
+    db = mongoClient.db(config.mongoDbName);
+    console.log('Connecxion à MongoDB reussie');
+  } catch (error) {
+    console.error('Erreur de la connexion à MongoDB:', error);
+    throw error;
+  }
 }
 
 async function connectRedis() {
-  // TODO: Implémenter la connexion Redis
-  // Gérer les erreurs et les retries
+  try {
+    redisClient = redis.createClient({ url: config.redisUri });
+    redisClient.on('error', (err) => console.error('Erreur de client Redis', err));
+    await redisClient.connect();
+    console.log('Connection à Redis reussie');
+  } catch (error) {
+    console.error('Erreur de la connexion à Redis:', error);
+    throw error;
+  }
 }
 
 // Export des fonctions et clients
 module.exports = {
-  // TODO: Exporter les clients et fonctions utiles
+  connectMongo,
+  connectRedis,
+  getMongoClient: () => mongoClient,
+  getRedisClient: () => redisClient,
+  getDb: () => db
 };
